827e195475311e5bb629e611897a8c9f5910a860
Rburch <ryanjb420@gmail.com>
2020-02-03 18:46:06 -0500

    Made new subsystem for Intake Arm

--- src/main/java/frc/robot/subsystems/IntakeArmSub.java
+++ src/main/java/frc/robot/subsystems/IntakeArmSub.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,16 +7,16 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotIO;
+/**
+ * Add your docs here.
+ */
+public class IntakeArmSub extends SubsystemBase {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
 
-public class IntakeSub extends SubsystemBase {
-  /**
-   * Creates a new IntakeSub.
-   */
-  public IntakeSub() {
+  public IntakeArmSub() {
 
   }
 
@@ -25,8 +25,12 @@ public class IntakeSub extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  public static void intakeSpinWheels() {
-    RobotIO.intakeAndControl.set(1.0);
+  public static void intakeLower() {
+    RobotIO.intakeArm.set(1.0);
+  }
+
+  public static void intakeRaise() {
+    RobotIO.intakeArm.set(-1.0);
   }
   
 }
