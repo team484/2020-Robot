ab87ffa209c40b9676c19785d3544397692579fc
DactahWambat <lhofmann031902@gmail.com>
2020-02-04 20:22:44 -0500

    H

--- src/main/java/frc/robot/subsystems/LEDSub.java
+++ src/main/java/frc/robot/subsystems/LEDSub.java
@@ -8,32 +8,17 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotIO;
-import frc.robot.commands.JoystickDrive;
 
-
-
-public class DriveSub extends SubsystemBase {
+public class LEDSub extends SubsystemBase {
   /**
-   * Creates a new DriveSub.
+   * Creates a new LEDSub.
    */
-  public DriveSub() {
-    setDefaultCommand(new JoystickDrive(this));
-    RobotIO.rightMotor2.follow(RobotIO.rightMotor1);
-    RobotIO.rightMotor3.follow(RobotIO.rightMotor1);
-    RobotIO.leftMotor2.follow(RobotIO.leftMotor1);
-    RobotIO.leftMotor3.follow(RobotIO.leftMotor1);
-}
-   
+  public LEDSub() {
+
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-  
-public static void set(double speed, double rot)
- {
-RobotIO.difDrive.arcadeDrive(speed, rot);
-}
-
-  
 }
