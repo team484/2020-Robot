b560f79727a7437c602ccdaf7c6d353093c749c9
Rburch <ryanjb420@gmail.com>
2020-02-05 19:12:02 -0500

    Ben is smelly, fixed RobotIO/settings and elevatorsub

--- src/main/java/frc/robot/RobotIO.java
+++ src/main/java/frc/robot/RobotIO.java
@@ -14,7 +14,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Victor;
@@ -28,33 +28,34 @@ import frc.robot.subsystems.DriveSub;
  * Add your docs here.
  */
 public class RobotIO {
-   public static WPI_TalonFX leftMotor1 = new WPI_TalonFX(RobotSettings.LEFT_MOTOR_1_ID);
-   public static WPI_TalonFX leftMotor2 = new WPI_TalonFX(RobotSettings.LEFT_MOTOR_2_ID);
-   public static WPI_TalonFX leftMotor3 = new WPI_TalonFX(RobotSettings.LEFT_MOTOR_3_ID);
+   public static final WPI_TalonFX leftMotor1 = new WPI_TalonFX(RobotSettings.LEFT_MOTOR_1_ID);
+   public static final WPI_TalonFX leftMotor2 = new WPI_TalonFX(RobotSettings.LEFT_MOTOR_2_ID);
+   public static final WPI_TalonFX leftMotor3 = new WPI_TalonFX(RobotSettings.LEFT_MOTOR_3_ID);
 
-   public static WPI_TalonFX rightMotor1 = new WPI_TalonFX(RobotSettings.RIGHT_MOTOR_1_ID);
-   public static WPI_TalonFX rightMotor2 = new WPI_TalonFX(RobotSettings.RIGHT_MOTOR_2_ID);
-   public static WPI_TalonFX rightMotor3 = new WPI_TalonFX(RobotSettings.RIGHT_MOTOR_3_ID);
+   public static final WPI_TalonFX rightMotor1 = new WPI_TalonFX(RobotSettings.RIGHT_MOTOR_1_ID);
+   public static final WPI_TalonFX rightMotor2 = new WPI_TalonFX(RobotSettings.RIGHT_MOTOR_2_ID);
+   public static final WPI_TalonFX rightMotor3 = new WPI_TalonFX(RobotSettings.RIGHT_MOTOR_3_ID);
 
-   public static WPI_TalonSRX shooterMotor1 = new WPI_TalonSRX(RobotSettings.SHOOTER_MOTOR_1_ID);
-   public static WPI_TalonSRX shooterMotor2 = new WPI_TalonSRX(RobotSettings.SHOOTER_MOTOR_2_ID);
+   public static final WPI_TalonSRX shooterMotor1 = new WPI_TalonSRX(RobotSettings.SHOOTER_MOTOR_1_ID);
+   public static final WPI_TalonSRX shooterMotor2 = new WPI_TalonSRX(RobotSettings.SHOOTER_MOTOR_2_ID);
 
-   public static WPI_TalonFX ballConveyerHorizontal = new WPI_TalonFX(RobotSettings.HORIZONTAL_BALL_CONVEYOR_MOTOR_ID);
-   public static WPI_TalonFX ballConveyerVertical = new WPI_TalonFX(RobotSettings.VERTICAL_BALL_CONVEYOR_MOTOR_ID);
+   public static final WPI_TalonFX ballConveyerHorizontal = new WPI_TalonFX(RobotSettings.HORIZONTAL_BALL_CONVEYOR_MOTOR_ID);
+   public static final WPI_TalonFX ballConveyerVertical = new WPI_TalonFX(RobotSettings.VERTICAL_BALL_CONVEYOR_MOTOR_ID);
    
-   public static WPI_VictorSPX climberWheels = new WPI_VictorSPX(RobotSettings.CLIMBER_WHEELS_MOTOR_ID);
+   public static final WPI_VictorSPX climberWheels = new WPI_VictorSPX(RobotSettings.CLIMBER_WHEELS_MOTOR_ID);
 
-   public static WPI_VictorSPX intakeAndControl = new WPI_VictorSPX(RobotSettings.INTAKE_AND_CONTROL_PANEL_MOTOR_ID);
+   public static final WPI_VictorSPX intakeAndControl = new WPI_VictorSPX(RobotSettings.INTAKE_AND_CONTROL_PANEL_MOTOR_ID);
 
-   public static CANSparkMax intakeArm = new CANSparkMax(RobotSettings.INTAKE_ARM_MOTOR_ID, MotorType.kBrushless);
+   public static final CANSparkMax intakeArm = new CANSparkMax(RobotSettings.INTAKE_ARM_MOTOR_ID, MotorType.kBrushless);
 
-   public static CANSparkMax leftElevatorMotor = new CANSparkMax(RobotSettings.LEFT_ELEVATOR_MOTOR_ID, MotorType.kBrushless);
-   public static CANSparkMax rightElevatorMotor = new CANSparkMax(RobotSettings.RIGHT_ELEVATOR_MOTOR_ID, MotorType.kBrushless);
+   public static final CANSparkMax leftElevatorMotor = new CANSparkMax(RobotSettings.LEFT_ELEVATOR_MOTOR_ID, MotorType.kBrushless);
+   public static final CANSparkMax rightElevatorMotor = new CANSparkMax(RobotSettings.RIGHT_ELEVATOR_MOTOR_ID, MotorType.kBrushless);
 
-   public static DifferentialDrive difDrive = new DifferentialDrive(leftMotor1, rightMotor1);
-   public static Joystick driveStick = new Joystick(RobotSettings.DRIVE_PORT);
+   public static final DifferentialDrive difDrive = new DifferentialDrive(leftMotor1, rightMotor1);
 
-   public static Joystick operatorStick = new Joystick(RobotSettings.DRIVE_PORT_2);
+   public static final Servo clutchServo = new Servo(RobotSettings.CLUTCH_SERVO_PORT);
+
+   public static final Joystick driveStick = new Joystick(RobotSettings.DRIVE_STICK_PORT);
+   public static final Joystick operatorStick = new Joystick(RobotSettings.OP_STICK_PORT);
    
-
 }
--- src/main/java/frc/robot/subsystems/ElevatorSub.java
+++ src/main/java/frc/robot/subsystems/ElevatorSub.java
@@ -8,7 +8,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.ElevatorSub;
 import frc.robot.RobotIO;
+import frc.robot.RobotSettings;
 
 public class ElevatorSub extends SubsystemBase {
   /**
@@ -22,6 +24,13 @@ public class ElevatorSub extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+  public static void engageClutch(){
+    RobotIO.clutchServo.setAngle(RobotSettings.CLUTCH_ENGAGE_ANGLE);
+  }
+  public static void disengageClutch(){
+    RobotIO.clutchServo.setAngle(RobotSettings.CLUTCH_DISENGAGE_ANGLE);
+  }
+  
   public static void set(double speed) {
     RobotIO.leftElevatorMotor.setVoltage(speed);
   
