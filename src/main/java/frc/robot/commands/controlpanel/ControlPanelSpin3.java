6479255381b4b46b8ba09bea616e327eee85e639
Rburch <ryanjb420@gmail.com>
2020-02-20 21:11:53 -0500

    Added ControlPanelSpin3

--- src/main/java/frc/robot/commands/controlpanel/ControlPanelSpin3.java
+++ src/main/java/frc/robot/commands/controlpanel/ControlPanelSpin3.java
@@ -14,36 +14,44 @@ import frc.robot.subsystems.ControlPanelSpinnerSub;
 import frc.robot.subsystems.ControlPanelSpinnerSub.WheelColor;
 
 public class ControlPanelSpin3 extends CommandBase {
+  WheelColor startColor = WheelColor.UNKNOWN;
+  boolean flip = false;
+  int hits = 0;
   /**
    * Creates a new ControlPanelSpin3.
    */
-  public Color startColor;
-  public ControlPanelSpin3(ControlPanelSpinnerSub subsystem, Color imput) {
+  public ControlPanelSpin3(ControlPanelSpinnerSub subsystem) {
     addRequirements(subsystem);
-    this.startColor = imput;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled .
   @Override
   public void initialize() {
-    
+    startColor = ControlPanelSpinnerSub.getColor();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
+    ControlPanelSpinnerSub.set(0.5);
+    if (ControlPanelSpinnerSub.getColor() == startColor) {
+      if (flip)
+        hits++;
+      flip = false;
+    } else
+      flip = true;
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    ControlPanelSpinnerSub.set(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return hits >= 6;
   }
 }
